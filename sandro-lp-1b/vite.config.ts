7510b116bc53976cd171582becc24325fec2e1d6
EDUARDO ROCHA TOLEDO CARVALHO <eduardo.rtcarvalho@gmail.com>
2024-11-21 11:35:23 -0300

    2b

--- sandro-lp-1b/vite.config.ts
+++ sandro-lp-1b/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://us-central1-email-sendgrid-442403.cloudfunctions.net/sendgridemail',
+        changeOrigin: true,
+        secure: true,
+
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
